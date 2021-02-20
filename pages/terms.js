85b1fed2d79d08cc14a5a6b0b6b66f0e933acfac
Poghosyan Hayk <poghosyanhayk1@gmail.com>
2021-02-20 18:49:11 +0400

    Fixed minor issue

--- pages/terms.js
+++ pages/terms.js
@@ -1,11 +1,9 @@
 import { withAuthServerSideProps, withAuthComponent } from 'components/withAuth';
-import Terms from 'features/Terms';
-import absoluteUrl from 'next-absolute-url';
+import Terms from 'features/terms';
 import { store } from 'store';
 
 export default withAuthComponent(Terms);
 export const getServerSideProps = withAuthServerSideProps(async (context, user = store.user) => {
-  const { origin } = absoluteUrl(context.req);
   try {
     return {
       props: {
