e59de731102da6b8c2cf6d91c78e4fc700420498
Poghosyan Hayk <poghosyanhayk1@gmail.com>
2021-02-20 18:45:40 +0400

    fixed minor issue

--- pages/content.js
+++ pages/content.js
@@ -1,11 +1,9 @@
 import { withAuthServerSideProps, withAuthComponent } from 'components/withAuth';
-import Content from 'features/Content';
-import absoluteUrl from 'next-absolute-url';
+import Content from 'features/content';
 import { store } from 'store';
 
 export default withAuthComponent(Content);
 export const getServerSideProps = withAuthServerSideProps(async (context, user = store.user) => {
-  const { origin } = absoluteUrl(context.req);
   try {
     return {
       props: {
