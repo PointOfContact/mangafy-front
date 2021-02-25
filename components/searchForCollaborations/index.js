075107c8059546a2b1fee07af9b1ab3f74ea8987
Belkin <rastnik1991@gmail.com>
2021-02-25 17:33:45 +0200

    components/filter-select

--- components/searchForCollaborations/index.js
+++ components/searchForCollaborations/index.js
@@ -8,7 +8,13 @@ import Close from '../icon/Close';
 import SvgSearch from '../icon/Search';
 import styles from './styles.module.scss';
 
-const SearchForCollaborations = ({}) => (
+import SelectFilter from 'components/ui-elements/select-filter';
+
+import PropTypes from 'prop-types';
+
+import { allCollaborations } from 'helpers/constant';
+
+const SearchForCollaborations = ({ type, onChange, onSubmit, isLogin, disabled = false }) => (
   <>
     <div className={styles.box}>
       <div className={'container'}>
@@ -23,74 +29,48 @@ const SearchForCollaborations = ({}) => (
               name="search"
               placeholder="Search for collaborations"
             />
+            <button type="submit" className={styles.box__search_submit}>
+              <SvgSearch width="22" height="22" />
+            </button>
           </form>
           <div className={styles.box__nav}>
-            <button className={cn(styles.box__nav_button)}>
-              <span className={styles.box__nav_button__text}>All collaboration</span>
-              <i className={styles.box__nav_button__icon}>
-                <BottomArrow width="17" height="10" />
-              </i>
-            </button>
-            <button className={cn(styles.box__nav_button, styles.box__nav_button__active)}>
-              <span className={styles.box__nav_button__text}>All Fields</span>
-              <i className={styles.box__nav_button__icon}>
-                <BottomArrow width="17" height="10" />
-              </i>
-            </button>
-            <div className={styles.box__nav_menu}>
-              <button className={styles.box__nav_menu__close}>
-                <Close width="12" height="12" />
-              </button>
-              <ul className={styles.box__nav_menu__list}>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link}>Writer</button>
-                  </Link>
-                </li>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link}>Illustrator</button>
-                  </Link>
-                </li>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link} disabled>
-                      Editor
-                    </button>
-                  </Link>
-                </li>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link} disabled>
-                      Mentor
-                    </button>
-                  </Link>
-                </li>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link}>Publisher</button>
-                  </Link>
-                </li>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link} disabled>
-                      Reviewer
-                    </button>
-                  </Link>
-                </li>
-                <li className={styles.box__nav_menu__list_item}>
-                  <Link href="#">
-                    <button className={styles.box__nav_menu__list_link} disabled>
-                      Translator
-                    </button>
-                  </Link>
-                </li>
-              </ul>
-            </div>
+            <SelectFilter
+              className={styles.box__nav_select}
+              id="type"
+              name="type"
+              onChange={onChange}
+              placeholder="All collaboration"
+              value={type || undefined}
+              options={allCollaborations}
+            />
+            <SelectFilter
+              className={styles.box__nav_select}
+              id="type"
+              name="type"
+              onChange={onChange}
+              placeholder="All Fields"
+              value={type || undefined}
+              options={allCollaborations}
+            />
           </div>
         </div>
       </div>
     </div>
   </>
 );
+
+SearchForCollaborations.propTypes = {
+  type: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  errorMessage: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  isLogin: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool,
+};
+
+SearchForCollaborations.defaultProps = {
+  type: null,
+  disabled: false,
+};
+
 export default SearchForCollaborations;
