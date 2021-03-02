a1fa2025dfc941117ed9c5c3e66c0fbf85182f84
Dwin Technology <dwin@Dwins-Mac-mini.local>
2021-03-02 18:19:39 +0400

    Added to Top button functionality

--- components/ui-elements/button-toTop/index.js
+++ components/ui-elements/button-toTop/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import styles from './styles.module.scss';
 
-const ButtonToTop = ({}) => (
-  <div className={styles.button}>
-    <div className={styles.button__circle}>
-      <img className={styles.button__circle_img} src="/img/to-top.svg" alt="" />
+const ButtonToTop = () => {
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+  return (
+    <div className={styles.button} onClick={scrollTop}>
+      <div className={styles.button__circle}>
+        <img className={styles.button__circle_img} src="/img/to-top.svg" alt="" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default ButtonToTop;
