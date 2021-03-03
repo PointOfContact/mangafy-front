7c219a2147546112bba00083ce384ea48bac48fa
Poghosyan Hayk <poghosyanhayk1@gmail.com>
2021-03-03 13:38:00 +0400

    Fixed bug

--- components/profile/profileContent/tabCommissionPricing/index.js
+++ components/profile/profileContent/tabCommissionPricing/index.js
@@ -11,7 +11,7 @@ const TabCommissionPricing = (props) => {
   return (
     <div className="content_tab_profile_1">
       <h3 className={styles.content_tab_profile_1__title}>Commision List</h3>
-      {user._id === profile?._id && (
+      {user && user._id === profile?._id && (
         <p className={styles.content_tab_profile_1__sub_title}>
           We believe that your talent is worth money, but how much, it's up to you, tell the whole
           world about it =) If you miised service, please let us know, and we will add it!
--- features/profile/index.js
+++ features/profile/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+import cn from 'classnames';
 import Footer from 'components/footer';
+import FooterPolicy from 'components/footer-policy';
 import Header from 'components/header';
 import ProfileContent from 'components/profile/profileContent';
 import ProfileOpenCollabs from 'components/profile/profileOpenCollabs';
 import ProfileTopBar from 'components/profile/profileTopBar';
-import PropTypes from 'prop-types';
-import cn from 'classnames';
 import ButtonToTop from 'components/ui-elements/button-toTop';
-import FooterPolicy from 'components/footer-policy';
+import PropTypes from 'prop-types';
+
 import styles from './styles.module.scss';
 
 const Profile = (props) => {
@@ -21,54 +22,54 @@ const Profile = (props) => {
   const genres = profileGenres.map((item) => item.name);
   return (
     <>
-    <ButtonToTop />
-    <div className={'wrapper'}>
-      <div className={'content'}>
-        <Header path="profile" user={user} />
-        <main>
-          <section>
-            <div className="container">
-              <ProfileTopBar
-                {...{
-                  user,
-                  profile,
-                }}
-              />
-            </div>
-          </section>
-            
-          <section>
-              <div className={cn("container", styles.container_profile)}>
-              <ProfileContent
-                {...{
-                  user,
-                  mangaStories,
-                  profile,
-                  total,
-                  profileGenres,
-                  genres,
-                }}
-              />
-            </div>
-          </section>
-            
-          <section>
-            <div className="container">
-              <ProfileOpenCollabs
-                {...{
-                  total,
-                  mangaStories,
-                  profile,
-                }}
-              />
-            </div>
-          </section>
-        </main>
+      <ButtonToTop />
+      <div className={'wrapper'}>
+        <div className={'content'}>
+          <Header path="profile" user={user} />
+          <main>
+            <section>
+              <div className="container">
+                <ProfileTopBar
+                  {...{
+                    user,
+                    profile,
+                  }}
+                />
+              </div>
+            </section>
+
+            <section>
+              <div className={cn('container', styles.container_profile)}>
+                <ProfileContent
+                  {...{
+                    user,
+                    mangaStories,
+                    profile,
+                    total,
+                    profileGenres,
+                    genres,
+                  }}
+                />
+              </div>
+            </section>
+
+            <section>
+              <div className="container">
+                <ProfileOpenCollabs
+                  {...{
+                    total,
+                    mangaStories,
+                    profile,
+                  }}
+                />
+              </div>
+            </section>
+          </main>
+        </div>
+        <Footer />
+        <FooterPolicy />
       </div>
-      <Footer />
-      <FooterPolicy />
-    </div>
-  </>
+    </>
   );
 };
 
