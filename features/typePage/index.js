1e0e7a855ce7e35d9953c7a509e1461a6121dbb7
PoghosyanHayk <poghosyanhayk1@gmail.com>
2021-04-14 19:20:54 +0400

    Fixed bugs (#103)
    Co-authored-by: Dwin Technology <dwin@Dwins-Mac-mini.local>

--- features/typePage/index.js
+++ features/typePage/index.js
@@ -5,6 +5,7 @@ import FooterPolicy from 'components/footer-policy';
 import Header from 'components/header';
 import TypePage from 'components/type-content';
 import ButtonToTop from 'components/ui-elements/button-toTop';
+import Head from 'next/head';
 import PropTypes from 'prop-types';
 
 export default function LandingNew({
@@ -18,6 +19,24 @@ export default function LandingNew({
 }) {
   return (
     <>
+      <Head>
+        <title>MangaFY – From story buidling to a full digital release.</title>
+        <meta
+          name="description"
+          content="The digital hub designed to help you produce your very owm comic or manga. From story buidling to a full digital release."
+        />
+        <meta property="og:url" content="https://www.mangafy.club" />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:title"
+          content="Start your Graphic Novel Journey and Get Creative with MangaFY"
+        />
+        <meta
+          property="og:description"
+          content="The digital hub designed to help you produce your very owm comic or manga. From story buidling to a full digital release."
+        />
+        <meta property="og:image" content="https://i.postimg.cc/cCy8qTg7/manga.jpg" />
+      </Head>
       <ButtonToTop />
       <div className={'wrapper'}>
         <div className={'content'}>
