ab8a3d674cced70c3490363470dd34b25efdf127
Aren-Gevorgyan <areng4518@gmail.com>
2021-06-17 17:07:31 +0400

    initial commit

---
+++ components/header/modalInviteMembers/index.js
@@ -0,0 +1,128 @@
+import React, { useEffect } from 'react';
+
+import { Modal, Form } from 'antd';
+import SvgClose from 'components/icon/Close';
+import PrimaryButton from 'components/ui-elements/button';
+import PrimaryInput from 'components/ui-elements/input';
+import PropTypes from 'prop-types';
+
+import styles from './styles.module.scss';
+
+const ModalInviteMembers = ({ showModal, setShowModal }) => {
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    form.resetFields();
+  }, [showModal, form]);
+
+  return (
+    <Modal
+      className={styles.modal}
+      title="Invite to MangaFy"
+      centered
+      visible={showModal}
+      width={500}
+      closeIcon={
+        <span className={styles.close} onClick={() => setShowModal(false)}>
+          <SvgClose height="18px" width="18px" />
+        </span>
+      }
+      footer={[]}>
+      <div className={styles.inputContainer}>
+        <p>Email addresses:</p>
+        <Form name="dynamic_form_item" form={form} initialValues={{ emails: [''] }}>
+          <Form.List name="emails">
+            {(fields, { add, remove }) => (
+              <>
+                {fields.map((field, index) => (
+                  <Form.Item required={false} key={field.key}>
+                    <Form.Item
+                      {...field}
+                      validateTrigger={['onChange', 'onBlur']}
+                      rules={[
+                        {
+                          type: 'email',
+                          message: 'Email is not a valid email!',
+                        },
+                      ]}
+                      noStyle>
+                      <PrimaryInput
+                        validateStatus="error"
+                        className={styles.input}
+                        placeholder={'name example@com'}
+                        isFullWidth={true}
+                        isLinear={true}
+                        onChange={() => {
+                          if (fields.length - 1 === index) {
+                            add('', fields.length);
+                          }
+                        }}
+                      />
+                    </Form.Item>
+                    {!!index && (
+                      <div className={styles.deleteInputContainer}>
+                        <span className={styles.deleteInput} onClick={() => remove(index)}>
+                          <SvgClose height="18px" width="18px" />
+                        </span>
+                      </div>
+                    )}
+                  </Form.Item>
+                ))}
+              </>
+            )}
+          </Form.List>
+          {/* <Link href={''}>
+                  <span className={styles.addMany}>Add many at once</span>
+                </Link> */}
+          <Form.Item>
+            <PrimaryButton
+              onClick={() => {
+                console.log(form.getFieldsValue('emails'));
+              }}
+              htmlType="submit"
+              className={styles.send}
+              text={'Send Invitations'}
+            />
+          </Form.Item>
+        </Form>
+        <div className={styles.copyContainer}>
+          <PrimaryInput
+            id={'path'}
+            className={styles.copyInput}
+            placeholder={'https://mangafy.club/'}
+            isFullWidth={true}
+            isLinear={true}
+          />
+          <button
+            className={styles.copyButton}
+            onClick={() => {
+              const input = document.getElementById('path');
+              input.select();
+              document.execCommand('copy');
+            }}>
+            Copy
+          </button>
+        </div>
+        {/* <div className={styles.switchContainer}>
+          <label className={styles.switch}>
+            <input type="checkbox" defaultChecked />
+            <span className={cn(styles.slider, styles.round)}></span>
+          </label>
+          <span>Enable invite link and share with your teammates</span>
+        </div> */}
+      </div>
+    </Modal>
+  );
+};
+
+ModalInviteMembers.propTypes = {
+  showModal: PropTypes.bool.isRequired,
+  setShowModal: PropTypes.func.isRequired,
+};
+
+ModalInviteMembers.defaultProps = {
+  showModal: false,
+  setShowModal: () => {},
+};
+
+export default ModalInviteMembers;
--- components/menu-mobile-popover/index.js
+++ components/menu-mobile-popover/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MenuNotificationsBox from 'components/menu-notifications-box';
 import Link from 'next/link';
@@ -11,10 +11,14 @@ const MenuMobilePopover = ({
   user,
   unreadNotificationsId,
   notificationsCount,
-}) => (
-  <>
-    <div className={styles.box}>
-      {/* <div className={styles.box__title}>
+  showNotification,
+  setShowNotification,
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <div className={styles.box}>
+        {/* <div className={styles.box__title}>
         <p className={styles.box__title_text}>
           <Link href="/pricing">
             <span className={styles.box__go_to_pro}>
@@ -23,36 +27,58 @@ const MenuMobilePopover = ({
           </Link>
         </p>
       </div> */}
-      <div className={styles.box__subtitle}>
-        <Link href="/my-profile">
-          <a className={styles.box__libk}>Profile</a>
-        </Link>
-      </div>
-      <div className={styles.box__subtitle}>
-        {/* <Link href="/my-profile">
+        <div className={styles.box__subtitle}>
+          <Link href="/my-profile">
+            <a className={styles.box__libk}>Profile</a>
+          </Link>
+        </div>
+        {user && (
+          <span
+            className={styles.inviteContainer}
+            onClick={() => {
+              setShowModal(!showModal);
+              setShowNotification(!showNotification);
+            }}>
+            Invite members
+          </span>
+        )}
+        <div className={styles.box__subtitle}>
+          {/* <Link href="/my-profile">
           <a className={styles.box__libk}>Edit collaboration availability</a>
         </Link>
         <Link href="/my-profile">
           <a className={styles.box__libk}>Account Settings</a>
         </Link> */}
-        <span onClick={removeAllStorage}>
-          <a className={styles.box__libk}>Sign out</a>
-        </span>
+          <span onClick={removeAllStorage}>
+            <a className={styles.box__libk}>Sign out</a>
+          </span>
+        </div>
+        <MenuNotificationsBox
+          user={user}
+          unreadNotificationsId={unreadNotificationsId}
+          notificationsCount={notificationsCount}
+          showModal={showModal}
+          setShowModal={setShowModal}
+          showNotification={showNotification}
+          setShowNotification={setShowNotification}
+        />
       </div>
-      <MenuNotificationsBox
-        user={user}
-        unreadNotificationsId={unreadNotificationsId}
-        notificationsCount={notificationsCount}
-      />
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 MenuMobilePopover.propTypes = {
   removeAllStorage: PropTypes.func.isRequired,
   user: PropTypes.object.isRequired,
   unreadNotificationsId: PropTypes.array.isRequired,
   notificationsCount: PropTypes.number.isRequired,
+  showNotification: PropTypes.bool,
+  setShowNotification: PropTypes.func,
+};
+
+MenuMobilePopover.defaultProps = {
+  showNotification: false,
+  setShowNotification: () => {},
 };
 
 export default MenuMobilePopover;
