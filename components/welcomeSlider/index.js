da5308ad2f284282557c6998052361da112a3ce6
Dwin Technology <dwin@Dwins-Mac-mini.local>
2021-06-30 19:31:32 +0400

    fixed bugs

--- components/welcomeSlider/index.js
+++ components/welcomeSlider/index.js
@@ -1,117 +1,103 @@
 import React from 'react';
 
-import Imgix from 'components/imgix';
-import Slider from 'react-slick';
+import client from 'api/client';
 import cn from 'classnames';
+import Slider from 'react-slick';
 
 import styles from './styles.module.scss';
 
-const WelcomeSlider = ({}) => {
-    const settings = {  
-        infinite: true,
-        autoplay: true,
-        centerMode: false,
-        dots: false,
-        arrows: false,
-        autoplaySpeed: 2000,
-        variableWidth: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-		{
-			breakpoint: 767,
-			settings: {
-				centerMode: true
-			}
-		}
-		]
-    };
+const WelcomeSlider = () => {
+  const settings = {
+    infinite: true,
+    autoplay: true,
+    centerMode: false,
+    dots: false,
+    arrows: false,
+    autoplaySpeed: 2000,
+    variableWidth: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          centerMode: true,
+        },
+      },
+    ],
+  };
 
-    return (
-        <Slider {...settings}>
-            <div className={styles.sliderItem}>
-                <img src="img/welcome-slider-ico1.png" />
-                <div className={cn(styles.sliderItem__info, styles.sliderItem__infoOrangeDark)}>
-                    Hey! I am a reader
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico1.svg" />
-                    </div>
-                </div>
+  const users = [
+    {
+      id: '1',
+      src: 'cc4dd418bedc5d0563c4497b3755bb5abe1b173fb1e1c5199948ed7f4fffa1e7.png',
+      icon: 'icons/welcome-slider-ico1.svg',
+      type: 'reader',
+      navURL: '/',
+    },
+    {
+      id: '2',
+      src: 'a2e322ac4d2b2c08093857f09c959a8e692c1bf291cb7fd8e197a4d59317b11a.png',
+      icon: 'icons/welcome-slider-ico2.svg',
+      type: 'writer',
+      navURL: '/',
+    },
+    {
+      id: '3',
+      src: '256d58d8418e8208cfc477b96fba0e5bdf2c26556440cef25ce7eba9d14ecbde.png',
+      icon: 'icons/welcome-slider-ico3.svg',
+      type: 'artist',
+      navURL: '/',
+    },
+    {
+      id: '4',
+      src: '19a4026534069d413702cd4ea4208958731b68b48e85f1dcc7eb3991adf4b43d.png',
+      icon: 'icons/welcome-slider-ico1.svg',
+      type: 'reader',
+      navURL: '/',
+    },
+    {
+      id: '5',
+      src: '71df9c80a7f4fc18599e1342fac966c5163b4696dd88224476ee720bcf3bf487.jpeg',
+      icon: 'icons/welcome-slider-ico2.svg',
+      type: 'writer',
+      navURL: '/',
+    },
+    {
+      id: '6',
+      src: 'f9c89829fe01a891324c9e0cbc7339bb7a27053919df736a388528421918f845.jpeg',
+      icon: 'icons/welcome-slider-ico3.svg',
+      type: 'artist',
+      navURL: '/',
+    },
+    {
+      id: '8',
+      src: 'ea7a49675333bf7d7c83171c9e819d4434b8882c78798b8369ec994f827bfffd.png',
+      icon: 'icons/welcome-slider-ico2.svg',
+      type: 'writer',
+      navURL: '/',
+    },
+  ];
+
+  return (
+    <Slider {...settings}>
+      {users?.map((user) => (
+        // <Link key={user.id} href={user.navURL}>
+        //   <a>
+        <div key={user.id} className={styles.sliderItem}>
+          <img src={client.UPLOAD_URL + user.src} />
+          <div className={cn(styles.sliderItem__info, styles.sliderItem__infoOrangeDark)}>
+            Hey! I am a {user.type}
+            <div className={styles.sliderItem__ico}>
+              <img src={user.icon} />
             </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico2.png" />
-                <div className={cn(styles.sliderItem__info)}>
-                    Hey! I am a writer
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico2.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico3.png" />
-                <div className={cn(styles.sliderItem__info, styles.sliderItem__infoPrimaryLight)}>
-                    Hey! I'm an artist
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico3.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico4.png" />
-                <div className={cn(styles.sliderItem__info, styles.sliderItem__infoOrangeDark)}>
-                    Hey! I am a reader
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico1.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico2.png" />
-                <div className={cn(styles.sliderItem__info)}>
-                    Hey! I am a writer
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico2.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/welcome-slider-ico1.png" />
-                <div className={cn(styles.sliderItem__info, styles.sliderItem__infoOrangeDark)}>
-                    Hey! I am a reader
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico1.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico2.png" />
-                <div className={cn(styles.sliderItem__info, styles.sliderItem__infoPrimaryLight)}>
-                    Hey! I'm an artist
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico3.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico3.png" />
-                <div className={cn(styles.sliderItem__info)}>
-                    Hey! I am a writer
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico2.svg" />
-                    </div>
-                </div>
-            </div>
-            <div className={styles.sliderItem}>
-                <img src="img/slider-ico4.png" />
-                <div className={cn(styles.sliderItem__info, styles.sliderItem__infoOrangeDark)}>
-                    Hey! I am a writer
-                    <div className={styles.sliderItem__ico}>
-                        <img src="icons/welcome-slider-ico1.svg" />
-                    </div>
-                </div>
-            </div>
-        </Slider>
-    );
+          </div>
+        </div>
+        //   </a>
+        // </Link>
+      ))}
+    </Slider>
+  );
 };
 
 export default WelcomeSlider;
--- features/index/home/index.js
+++ features/index/home/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
-import { Spin } from 'antd';
 import cn from 'classnames';
-import Imgix from 'components/imgix';
-import LargeButton from 'components/ui-elements/large-button';
-import dynamic from 'next/dynamic';
 import PrimaryButton from 'components/ui-elements/button';
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
 
 import styles from './styles.module.sass';
 
-const SimpleSlider = dynamic(() => import('components/slider'), { loading: () => <Spin /> });
-
-const WelcomeSlider = dynamic(() => import('components/welcomeSlider'), {  });
+const WelcomeSlider = dynamic(() => import('components/welcomeSlider'), {});
 
 export default function Home() {
   return (
@@ -29,14 +24,16 @@ export default function Home() {
                 </p>
               </div>
               <div className={styles.main__buttons}>
-                <Link href="">
+                <Link href="sign-up">
                   <a className={cn('btn_submit')}>
                     <PrimaryButton text="Sign Up for free" className={styles.signUpFree} />
                   </a>
                 </Link>
-                <Link href="">
+                <Link href="/create-a-story/start">
                   <a className={styles.header__menu}>
-                    <PrimaryButton className={styles.joinBtn} text="Start a Project"></PrimaryButton>
+                    <PrimaryButton
+                      className={styles.joinBtn}
+                      text="Start a Project"></PrimaryButton>
                   </a>
                 </Link>
               </div>
@@ -52,7 +49,9 @@ export default function Home() {
         <div className={'container'}>
           <div className={styles.platformDescr__title}>One platform to call home.</div>
           <div className={styles.platformDescr__subtitle}>
-            MangaFY was built to help artists focus all their work in one place: collaborate, manage your manga or comic workflow, chat, set deadlines and tasks, and turn your story into a ready-to-be-published graphic novel.
+            MangaFY was built to help artists focus all their work in one place: collaborate, manage
+            your manga or comic workflow, chat, set deadlines and tasks, and turn your story into a
+            ready-to-be-published graphic novel.
           </div>
           <div className={styles.platformDescr__wrap}>
             <div className={styles.platformDescr__item}>
@@ -61,7 +60,8 @@ export default function Home() {
               </div>
               <div className={styles.platformDescr__itemTitle}>Grow as a graphic novelist</div>
               <div className={styles.platformDescr__itemDescr}>
-                Join a vivid community of creators (writers, illustrators, editors, translators, <br /> and inkers)
+                Join a vivid community of creators (writers, illustrators, editors, translators,{' '}
+                <br /> and inkers)
               </div>
             </div>
             <div className={cn(styles.platformDescr__item, styles.platformDescr__itemCenter)}>
@@ -79,7 +79,8 @@ export default function Home() {
               </div>
               <div className={styles.platformDescr__itemTitle}>Management Tools</div>
               <div className={styles.platformDescr__itemDescr}>
-                Manage the production process of your manga or comics and team workflow via dedicated tools 
+                Manage the production process of your manga or comics and team workflow via
+                dedicated tools
               </div>
             </div>
           </div>
@@ -90,7 +91,7 @@ export default function Home() {
         <div className={'container'}>
           <div className={styles.goinSection__join}>
             <div className={styles.goinSection__joinText}>Ready?</div>
-            <Link href="">
+            <Link href="sign-in">
               <a className={cn('btn_submit')}>
                 <PrimaryButton text="Login Now" className={styles.goinSection__goinNow} />
               </a>
@@ -107,11 +108,13 @@ export default function Home() {
 
       <div className={styles.titleBlock}>
         <div className={'container'}>
-          <div className={styles.titleBlock__title}>The future of graphic novel <br /> self-production.</div>
+          <div className={styles.titleBlock__title}>
+            The future of graphic novel <br /> self-production.
+          </div>
           <div className={styles.titleBlock__subtitle}>
-            We believe that in today's digital age, producing your own comic book or manga should not be
-            an obstacle.  As such, we have designed simple tools for aspiring and freelance artists to 
-            take their stories forward towards a visualized novel - in easy steps.
+            We believe that in today's digital age, producing your own comic book or manga should
+            not be an obstacle. As such, we have designed simple tools for aspiring and freelance
+            artists to take their stories forward towards a visualized novel - in easy steps.
           </div>
         </div>
       </div>
@@ -134,23 +137,33 @@ export default function Home() {
 
       <div className={styles.privAndSecure}>
         <div className={'container'}>
-          <div className={styles.privAndSecure__title}>Privately and securely work on <br /> your graphic novel idea. </div>
+          <div className={styles.privAndSecure__title}>
+            Privately and securely work on <br /> your graphic novel idea.{' '}
+          </div>
           <div className={styles.privAndSecure__wrap}>
             <div className={styles.privAndSecure__item}>
-              Our tools allow you to <br /> <span className={styles.privAndSecure__itemBold}>manage your storyboard</span> and page scripts, define your characters. 
+              Our tools allow you to <br />{' '}
+              <span className={styles.privAndSecure__itemBold}>manage your storyboard</span> and
+              page scripts, define your characters.
             </div>
             <div className={styles.privAndSecure__item}>
-              <span className={styles.privAndSecure__itemBold}>Grow with new teammates</span>,<br /> invite team members to your secure workspace and collaborate together by setting tasks and ease of communication. 
+              <span className={styles.privAndSecure__itemBold}>Grow with new teammates</span>,<br />{' '}
+              invite team members to your secure workspace and collaborate together by setting tasks
+              and ease of communication.
             </div>
             <div className={styles.privAndSecure__item}>
-              <span className={styles.privAndSecure__itemBold}>Seamlessly send files</span> between the team and exchange ideas until you have a finalized graphic novel at hand. 
+              <span className={styles.privAndSecure__itemBold}>Seamlessly send files</span> between
+              the team and exchange ideas until you have a finalized graphic novel at hand.
             </div>
           </div>
           <div className={styles.privAndSecure__subtitle}>
-            Once you are done working on your novel you can <span className={styles.privAndSecure__itemBold}>easily publish</span> it through MangaFY on any of the leading <br /> self-publishing platforms and begin getting followers and monetize your work.
+            Once you are done working on your novel you can{' '}
+            <span className={styles.privAndSecure__itemBold}>easily publish</span> it through
+            MangaFY on any of the leading <br /> self-publishing platforms and begin getting
+            followers and monetize your work.
           </div>
           <div className={styles.privAndSecure__textBold}>Simple and stright-foward.</div>
-          <Link href="">
+          <Link href="sign-up">
             <a className={cn('btn_submit')}>
               <PrimaryButton text="Try for free" className={styles.privAndSecure__try} />
             </a>
@@ -160,11 +173,15 @@ export default function Home() {
 
       <div className={styles.titleSlider}>
         <div className={'container'}>
-          <div className={styles.titleSlider__title}>Join our<br /> community today</div>
+          <div className={styles.titleSlider__title}>
+            Join our
+            <br /> community today
+          </div>
           <div className={styles.titleSlider__subtitle}>
-            Do you love graphic novels? Want to constantly stay inspired and be<br />
-            surrounded by like-minded creators? Then sign-up today and get rewarded for
-            your love <br />
+            Do you love graphic novels? Want to constantly stay inspired and be
+            <br />
+            surrounded by like-minded creators? Then sign-up today and get rewarded for your love{' '}
+            <br />
             of comics and manga
           </div>
         </div>
@@ -172,11 +189,11 @@ export default function Home() {
 
       <div className={styles.sliderWelcome}>
         <WelcomeSlider />
-        <Link href="">
-            <a className={cn('btn_submit')}>
-              <PrimaryButton text="Sign Up for free" className={styles.sliderWelcome__signup} />
-            </a>
-          </Link>
+        <Link href="sign-up">
+          <a className={cn('btn_submit')}>
+            <PrimaryButton text="Sign Up for free" className={styles.sliderWelcome__signup} />
+          </a>
+        </Link>
       </div>
 
       {/* <div className={cn('sliderMobile', styles.sliderInMobile)}>
@@ -189,7 +206,6 @@ export default function Home() {
           <SimpleSlider />
         </div>
       </div> */}
-
     </>
   );
 }
