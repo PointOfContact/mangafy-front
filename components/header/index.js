78bded1edc3ca7b651d3c6eb1a7d4975f5ca391c
Dwin Technology <dwin@Dwins-Mac-mini.local>
2021-07-28 20:19:56 +0400

    fix bug

--- components/header/index.js
+++ components/header/index.js
@@ -180,11 +180,7 @@ const Header = ({ user, path }) => {
               ) : (
                 <Link href="/sign-in">
                   <a>
-                    <Imgix
-                      layout="fill"
-                      src="https://mangafy.club/img/header-log-in.svg"
-                      alt="MangaFy login"
-                    />
+                    <img src="/img/header-log-in.svg" alt="MangaFy login" />
                     {/* TODO: chage to svg component */}
                   </a>
                 </Link>
