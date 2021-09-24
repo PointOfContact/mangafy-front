7c1819c1c643b46e79e25556b4f427a3761b447a
Aren-gevorgyan <areng4518@gmail.com>
2021-09-24 11:26:46 +0400

    replace background-images on imgix (feed)

--- components/type-content/discussionLeftBar/happeningCard/index.js
+++ components/type-content/discussionLeftBar/happeningCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import client from 'api/client';
+import Imgix from 'components/imgix';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
@@ -15,11 +16,14 @@ const HappeningCard = (props) => {
         <a className={styles.href}>
           <li className={styles.tagsListItem}>
             <div className={styles.tagsDescr}>
-              <div
+              <Imgix
                 className={styles.tagsImages}
-                style={{
-                  backgroundImage: `url(${img ? client.UPLOAD_URL + img : '/img/mangastory.jpg'})`,
-                }}></div>
+                layout="fixed"
+                width={35}
+                height={43}
+                src={img ? client.UPLOAD_URL + img : 'https://mangafy.club/img/mangastory.webp'}
+                alt="MangaFy club tags"
+              />
               <div className={styles.tagsDescrText}>{title}</div>
             </div>
             <div className={styles.tagsCount}>
