4cec9e3f26564275577d3f1b6ac8273fe26d289d
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-09-30 12:37:51 +0400

    fix bugs (#283)
    not create hero card if name value only spaces
    description height reduce
    set scroll on All-Genres options

--- components/searchForCollaborations/index.js
+++ components/searchForCollaborations/index.js
@@ -178,12 +178,7 @@ const SearchForCollaborations = (props) => {
                 value={selectedTypes || []}
                 onChange={handleArtistClick}
                 dropdownClassName="select-filter"
-                className={cn(
-                  styles.box__nav_select,
-                  styles.box__nav_artistType,
-                  !selectedTypes?.length && styles.box__nav_disabled_scroll,
-                  'select-filter'
-                )}>
+                className={cn(styles.box__nav_select, 'select-filter')}>
                 {artistOptions(userTypes)}
               </Select>
               <div className={styles.allGenres} id="area">
