e395a024998d6537e27d8f1c2c950e7984c8559d
Aren-gevorgyan <areng4518@gmail.com>
2021-10-14 15:35:25 +0400

    set routing

--- components/modalCreateProject/index.js
+++ components/modalCreateProject/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Modal } from 'antd';
+import { Button, Modal, notification } from 'antd';
+import client from 'api/client';
+import cn from 'classnames';
 import SvgAllowLeft from 'components/icon/AllowLeft';
 import SvgClose from 'components/icon/Close';
 import PrimaryInput from 'components/ui-elements/input';
@@ -8,30 +10,83 @@ import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
 
-const ModalCreateProject = ({ createProjectModal, showCreateProjectModal }) => (
-  <Modal
-    className={styles.modalCreateProject}
-    zIndex={1000000}
-    closeIcon={
-      <span className={styles.close}>
-        <SvgClose />
-      </span>
-    }
-    onCancel={() => {
-      showCreateProjectModal(false);
-    }}
-    visible={createProjectModal}
-    footer={null}>
-    <h1>Create webcomics project</h1>
-    <p>Collaborate, manage projects get donations, and reach new productivity peaks.</p>
-    <div className={styles.containerCreateProject}>
-      <PrimaryInput placeholder="Project Name" />
-      <button>
-        Start my webcomics <SvgAllowLeft />{' '}
-      </button>
-    </div>
-  </Modal>
-);
+const ModalCreateProject = ({ createProjectModal, showCreateProjectModal }) => {
+  const [title, setTitle] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [change, setChange] = useState(false);
+
+  useEffect(() => {
+    setChange(false);
+    setTitle('');
+  }, [createProjectModal]);
+
+  const createMangaStory = () => {
+    const data = { title };
+    const jwt = client.getCookie('feathers-jwt');
+    setLoading(true);
+    import('api/restClient').then((m) => {
+      m.default
+        .service('/api/v2/manga-stories')
+        .create(data, {
+          headers: { Authorization: `Bearer ${jwt}` },
+        })
+        .then((res) => {
+          showCreateProjectModal(false);
+          setLoading(false);
+          window.open(`/manga-story/${res._id}?create=true`, '_self');
+        })
+        .catch((err) => {
+          setLoading(false);
+          notification.error({
+            message: err.message,
+            placement: 'bottomLeft',
+          });
+          return err;
+        });
+    });
+  };
+
+  const errorMessage = (className) =>
+    title?.trim()?.length < 2 &&
+    change && (
+      <p className={cn(styles.error, className)}>
+        You need a unique title that fits your webcomic and makes a wow effect{' '}
+      </p>
+    );
+
+  return (
+    <Modal
+      className={styles.modalCreateProject}
+      closeIcon={
+        <span className={styles.close}>
+          <SvgClose />
+        </span>
+      }
+      onCancel={() => {
+        showCreateProjectModal(false);
+      }}
+      visible={createProjectModal}
+      footer={null}>
+      <h1 className={styles.title}>Create webcomics project</h1>
+      <p className={styles.description}>
+        Collaborate, manage projects get donations, and reach new productivity peaks.
+      </p>
+      <div className={styles.containerCreateProject}>
+        <PrimaryInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          onMouseOut={() => setChange(true)}
+          placeholder="Project Name"
+        />
+        {errorMessage(styles.errorMobile)}
+        <Button loading={loading} onClick={() => title?.trim()?.length > 1 && createMangaStory()}>
+          Start my webcomics <SvgAllowLeft />
+        </Button>
+      </div>
+      {errorMessage()}
+    </Modal>
+  );
+};
 
 ModalCreateProject.propTypes = {
   createProjectModal: PropTypes.bool.isRequired,
