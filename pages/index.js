dded7116534f4365eef40aefca6f8a1ba92a8831
Poghosyan <poghosyanhayk1@gmail.com>
2021-10-28 17:02:05 +0400

    Added logs

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,9 @@ export default withAuthComponent(MiddlewareIndexPage);
 
 export const getServerSideProps = withAuthServerSideProps(async (context, user = null, jwt) => {
   const viewUrlName = context?.req?.headers?.host;
+  console.log('viewUrlName', viewUrlName);
   const getNameViewUrl = !!viewUrlName && viewUrlName.split('.').reverse()[2];
+  console.log('getNameViewUrl', getNameViewUrl);
 
   if (!getNameViewUrl) {
     return {
@@ -21,14 +23,17 @@ export const getServerSideProps = withAuthServerSideProps(async (context, user =
     const mangaStory = await client.service('/api/v2/manga-stories').find({
       query: { viewUrlName: getNameViewUrl },
     });
+    console.log('mangaStory', mangaStory);
 
     const storyBoard = await client.service('/api/v2/story-boards').find({
       query: {
         mangaStoryId: mangaStory?.data[0]?._id,
       },
     });
+    console.log('storyBoard', storyBoard);
+    console.log('process.env', process.env);
 
-    if (!mangaStory?.data?.length && process.env.NEXT_REDIRECT_ENABLED) {
+    if (!mangaStory?.data?.length && process.env.NEXT_PUBLIC_REDIRECT_ENABLED) {
       context.res.writeHead(301, {
         Location: 'https://mangafy.club',
       });
@@ -37,6 +42,13 @@ export const getServerSideProps = withAuthServerSideProps(async (context, user =
         props: {},
       };
     }
+    console.log('process.env', {
+      // user: user || store.user,
+      storyBoardId: storyBoard?.data[0]?._id,
+      mangaUrls: storyBoard?.data[0]?.mangaUrls,
+      mangaStoryId: mangaStory?.data[0]?._id,
+      mangaStoryTitle: mangaStory?.data[0]?.title,
+    });
 
     return {
       props: {
