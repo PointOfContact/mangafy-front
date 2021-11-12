1c087bf59c9ab2ce460630e5aba2d73d1456adae
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-11-12 14:35:08 +0400

    add published in chapters page (#361)

--- features/mangaStory/components/chapter/chapterItems/index.js
+++ features/mangaStory/components/chapter/chapterItems/index.js
@@ -1,101 +1,29 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 import SvgAdd from 'components/icon/Add';
-import SvgDelete from 'components/icon/Delete';
-import SvgPurplePencil from 'components/icon/PurplePencil';
-import Popconfirm from 'components/popconfirm';
-import PrimaryInput from 'components/ui-elements/input';
-import mangaStoryAPI from 'features/mangaStory/mangaStoryAPI';
 import PropTypes from 'prop-types';
 
 import Pages from '../pages';
+import ChapterHeader from './chapterHeader';
 import ModalCreatePage from './modalCreatePage';
 import styles from './styles.module.scss';
 
 const ChapterItems = ({ chapters, setChapters, storyBoard }) => {
-  const [editName, setEditName] = useState('');
-  const [mouseOut, onMouseOut] = useState(false);
   const [visibleModal, setVisibleModal] = useState(false);
   const [chapterItem, setChapterItem] = useState({});
   const [pageItem, setPageItem] = useState({});
   const [modalTitle, setModalTitle] = useState('Create page');
-  const inputRef = useRef(null);
-  const [edit, setEdit] = useState('');
-  const validate = editName.trim().length < 2;
-
-  useEffect(() => {
-    // open input get data
-    !!edit &&
-      inputRef.current.focus({
-        cursor: 'end',
-      });
-  }, [edit]);
-
-  const upgradeChapterData = (item, resultId) =>
-    chapters.map((value) => (value?._id === resultId ? item : value));
-
-  const error = editName.trim().length < 2 && mouseOut && (
-    <p className={styles.error}>
-      Please enter a name for your chapter. You can leave it blank if you want
-    </p>
-  );
 
   return (
     <>
       {chapters?.map((value, index) => (
         <div key={value._id}>
-          <div className={styles.titleContainer}>
-            {edit === value?._id ? (
-              <div className={styles.inputNameContainer}>
-                <PrimaryInput
-                  inputRef={inputRef}
-                  value={editName}
-                  maxLength={30}
-                  placeholder="Chapter name"
-                  className={styles.chapterNameInput}
-                  onChange={(e) => setEditName(e.target.value)}
-                  onMouseOut={() => onMouseOut(true)}
-                  onBlur={() => {
-                    validate
-                      ? setEdit(false)
-                      : mangaStoryAPI.chapter.patch(
-                          value?._id,
-                          setEdit,
-                          editName,
-                          upgradeChapterData,
-                          setChapters
-                        );
-                  }}
-                />
-                {error}
-              </div>
-            ) : (
-              <div
-                onClick={() => {
-                  setEdit(value?._id);
-                  onMouseOut(false);
-                  setEditName(value?.title);
-                }}
-                className={styles.chapterName}>
-                <h2 className={styles.chapterTitle}>{value?.title}</h2>
-                <SvgPurplePencil width={25} height={25} />
-              </div>
-            )}
-
-            <Popconfirm
-              overlayClassName={styles.popConfirm}
-              position={'right'}
-              title="Are you sure to delete this chapter"
-              onConfirm={() => {
-                mangaStoryAPI.chapter.delete(value._id, index, chapters, setChapters);
-              }}
-              item={
-                <span className={styles.deleteChapter}>
-                  <SvgDelete width={15} height={15} />
-                </span>
-              }
-            />
-          </div>
+          <ChapterHeader
+            value={value}
+            setChapters={setChapters}
+            index={index}
+            chapters={chapters}
+          />
           <div className={styles.chapterItem}>
             <div className={styles.addPageContainer}>
               <div
