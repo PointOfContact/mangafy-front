f98b5c1f90f93948b53db0e1fbae17a133db275d
Aren <63490565+Aren-Gevorgyan@users.noreply.github.com>
2021-11-21 22:59:00 +0400

    create ChapterTitle component (#380)
    * create ChapterTitle component
    
    * change chapter style

--- features/mangaStory/components/chapter/chapterItems/index.js
+++ features/mangaStory/components/chapter/chapterItems/index.js
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 import SvgAdd from 'components/icon/Add';
-import SvgMobileMenu from 'components/icon/MobileMenu';
-import ToggleSwitch from 'components/ui-elements/toggleSwitch';
-import mangaStoryAPI from 'features/mangaStory/mangaStoryAPI';
 import PropTypes from 'prop-types';
 
 import Pages from '../pages';
-import ChapterHeader from './chapterHeader';
+import ChapterFooter from './chapterFooter';
+import ChapterTitle from './chapterTitle';
 import ModalCreatePage from './modalCreatePage';
 import styles from './styles.module.scss';
 
@@ -15,44 +13,23 @@ const ChapterItems = ({ chapters, setChapters, storyBoard }) => {
   const [visibleModal, setVisibleModal] = useState(false);
   const [chapterItem, setChapterItem] = useState({});
   const [pageItem, setPageItem] = useState({});
-  const [publish, setPublish] = useState(false);
+  const [edit, setEdit] = useState('');
   const [modalTitle, setModalTitle] = useState('Create page');
-  const publishedRef = useRef(null);
-
-  useEffect(() => {
-    // publishedRef.current.checked = value?.published;
-  }, []);
-
-  const upgradeChapterData = (item, resultId) =>
-    chapters.map((val) => (val?._id === resultId ? item : val));
-
-  const publishedChapter = (value) => {
-    const publishedValue = publishedRef.current.checked;
-    setPublish(publishedValue);
-    console.log(publishedValue);
-    mangaStoryAPI.chapter.patch(
-      value?._id,
-      { published: publishedValue },
-      upgradeChapterData,
-      () => {},
-      setChapters
-    );
-  };
 
   return (
-    <>
+    <div className={styles.container}>
       {chapters?.map((value, index) => (
         <div key={value._id}>
-          <ChapterHeader
-            value={value}
-            setChapters={setChapters}
-            index={index}
-            chapters={chapters}
-          />
           <div className={styles.chapterItem}>
             <div className={styles.addPageContainer}>
               <div className={styles.addPage}>
-                <h3>{value.title}</h3>
+                <ChapterTitle
+                  value={value}
+                  chapters={chapters}
+                  setChapters={setChapters}
+                  edit={edit}
+                  setEdit={setEdit}
+                />
                 <div
                   className={styles.newPageContainer}
                   onClick={() => {
@@ -64,18 +41,13 @@ const ChapterItems = ({ chapters, setChapters, storyBoard }) => {
                   <SvgAdd width={50} height={50} />
                   <h4>New Page</h4>
                 </div>
-                <div className={styles.chapterItemFooter}>
-                  <div className={styles.switchContainer}>
-                    <ToggleSwitch
-                      onChange={() => publishedChapter(value)}
-                      inputRef={publishedRef}
-                      className={styles.switch}
-                      value={value.published}
-                    />
-                    {publish ? 'Published' : 'Draft'}
-                  </div>
-                  <SvgMobileMenu width="20px" height="20px" />
-                </div>
+                <ChapterFooter
+                  value={value}
+                  setChapters={setChapters}
+                  index={index}
+                  chapters={chapters}
+                  setEdit={setEdit}
+                />
               </div>
             </div>
             <div className={styles.chapterContainer}>
@@ -106,7 +78,7 @@ const ChapterItems = ({ chapters, setChapters, storyBoard }) => {
         modalTitle={modalTitle}
         pageItem={pageItem}
       />
-    </>
+    </div>
   );
 };
 
