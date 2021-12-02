973f5b17c58d272d7b248e519c8db51e4a869a0b
Aren-gevorgyan <areng4518@gmail.com>
2021-12-02 19:32:47 +0400

    change view page style

--- features/mangaView/index.js
+++ features/mangaView/index.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import client from 'api/client';
+import cn from 'classnames';
 import FooterPolicy from 'components/footer-policy';
 import Imgix from 'components/imgix';
+import { ShareButtons } from 'components/share';
 import Pagination from 'components/ui-elements/pagination';
 import { NextSeo } from 'next-seo';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 
 import styles from './styles.module.scss';
@@ -15,23 +18,86 @@ const PDFViewer = dynamic(() => import('components/pdfViewer'), {
   ssr: false,
 });
 
-const MangaView = ({ user, storyBoardId, mangaUrls, mangaStoryId, mangaStoryTitle, chapters }) => {
-  const [currentChapter, setCurrentChapter] = useState(1);
+const MangaView = ({
+  user,
+  storyBoardId,
+  mangaUrls,
+  mangaStoryId,
+  mangaStoryTitle,
+  chapters,
+  userName,
+  participants,
+}) => {
+  const router = useRouter();
+  const currentChapterNumber = +router.query.chapter;
+  const [currentChapter, setCurrentChapter] = useState(currentChapterNumber);
+  const [images, setImages] = useState([]);
 
-  const images = mangaUrls?.map((value) => {
-    const imgType = !!value.imageUrl ? value.imageUrl.slice(-3) : value.slice(-3);
-    const ifPdf = imgType === 'pdf' || imgType === 'PDF';
+  console.log(userName, participants, 'participants');
 
+  useEffect(() => {
+    const getCurrentMangaUrls = chapters[currentChapter - 1].mangaUrls;
+    const chapterImages = getCurrentMangaUrls.map((value) => {
+      const imgType = !!value.imageUrl ? value.imageUrl.slice(-3) : value.slice(-3);
+      const ifPdf = imgType === 'pdf' || imgType === 'PDF';
+
+      return (
+        <div className={styles.containerImages} key={value}>
+          {ifPdf ? (
+            <PDFViewer url={client.UPLOAD_URL + value} />
+          ) : (
+            <Imgix layout="fill" src={client.UPLOAD_URL + value} alt="Mangafy preview images" />
+          )}
+        </div>
+      );
+    });
+
+    setImages(chapterImages);
+  }, [currentChapter]);
+
+  useEffect(() => {
+    router.push(
+      `http://localhost:3000/manga-view/${storyBoardId}?chapter=${currentChapter}`,
+      undefined,
+      { shallow: true }
+    );
+  }, [currentChapter]);
+
+  const chapterItems = chapters.map((value, index) => {
+    const type = value.cover.slice(-3);
+    const ifPdf = type === 'pdf' || type === '{DF';
+    const activeChapter = index + 1 === currentChapter;
     return (
-      <div className={styles.containerImages} key={value}>
+      <div
+        className={cn(styles.itemChapters, activeChapter && styles.activeChapter)}
+        key={value._id + index}
+        onClick={() => setCurrentChapter(index + 1)}>
         {ifPdf ? (
-          <PDFViewer url={client.UPLOAD_URL + value} />
+          <PDFViewer url={client.UPLOAD_URL + value.cover} />
         ) : (
-          <Imgix layout="fill" src={client.UPLOAD_URL + value} alt="Mangafy preview images" />
+          <Imgix
+            width={100}
+            height={100}
+            src={client.UPLOAD_URL + value.cover}
+            alt="MangaFy chapter image"
+          />
         )}
+        <p>CH {index + 1}</p>
+        <div className={styles.opacity} />
       </div>
     );
   });
+
+  const participantItems = participants.map((value, index) => (
+    <div key={value + index} className={styles.participantsContainer}>
+      <Imgix
+        width={50}
+        height={50}
+        src={client.UPLOAD_URL + value.avatar}
+        alt={'MangaFy participants image'}
+      />
+    </div>
+  ));
 
   return (
     <>
@@ -71,8 +137,22 @@ const MangaView = ({ user, storyBoardId, mangaUrls, mangaStoryId, mangaStoryTitl
           currentChapter={currentChapter}
           setCurrentChapter={setCurrentChapter}
         />
-        <div className={styles.imagesContainer}>{images}</div>
+        <div className={styles.menu}>
+          <div className={styles.imagesContainer}>{images}</div>
+          <p className={styles.shareDescription}>
+            Share this series and show support for the creator!
+          </p>
+          <ShareButtons
+            className={styles.shareButtons}
+            shareUrl={`${client.API_ENDPOINT}/manga-view/${storyBoardId}`}
+          />
+          {/* <div className={styles.userData}>
+            <p className={styles.shareDescription}>{userName}</p>
+            {participantItems}
+          </div> */}
+        </div>
         <div className={styles.footer}>
+          <div className={styles.chaptersItems}>{chapterItems}</div>
           <div className={styles.paginationContainer}>
             <Pagination
               currentNumber={currentChapter}
@@ -94,6 +174,8 @@ MangaView.propTypes = {
   mangaStoryId: PropTypes.string.isRequired,
   mangaStoryTitle: PropTypes.string.isRequired,
   chapters: PropTypes.array.isRequired,
+  userName: PropTypes.string.isRequired,
+  participants: PropTypes.array.isRequired,
 };
 
 MangaView.defaultProps = {
