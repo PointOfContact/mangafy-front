b01cc63cbf58573f883b73655afe5a86a2569cfb
Vafflee <chelovekosa@gmail.com>
2022-10-05 18:06:22 +0300

    Made separated MangaComment component and added senderInfo support

--- components/shotComments/MangaComments.js
+++ components/shotComments/MangaComments.js
@@ -7,40 +7,19 @@ import styles from './styles.module.scss';
 import { notification } from 'antd';
 import Close from 'components/icon/new/Close';
 import Send from 'components/icon/new/Send';
-import Avatar from 'components/Avatar';
 import cn from 'classnames';
+import MangaComment from './MangaComment';
 
 const MangaComments = ({ className, comments = [], createComment }) => {
-  //   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
   const inputRef = useRef(null);
-
-  //   useEffect(() => {
-  //     updateComments();
-  //   }, [manga]);
 
   function clearInput() {
     setNewComment('');
     if (inputRef.current) inputRef.current.innerHTML = '';
   }
 
-  //   function updateComments() {
-  //     client.service('/api/v2/comments').find({
-  //         query: {
-  //           mangaStoryId: manga.id,
-  //           $sort: { createdAt: -1 },
-  //           $limit: 1000,
-  //         },
-  //       });
-  //       .then((res) => {
-  //         setComments(res.data.reverse());
-  //         onUpload && onUpload();
-  //       })
-  //       .catch((err) => console.log(err));
-  //   }
-
   function handleChange(e) {
-    // eslint-disable-next-line no-shadow
     const value = e.target.innerText;
     setNewComment(value);
   }
@@ -61,37 +40,7 @@ const MangaComments = ({ className, comments = [], createComment }) => {
   let commentsElements = [];
   if (Array.isArray(comments)) {
     commentsElements = comments?.map((comment) => (
-      <div className={styles.comment} key={comment._id}>
-        <div className={styles.comment__body}>
-          <div className={styles.comment__avatar}>
-            {/* {comment.authorInfo?.avatar ? (
-              <Imgix width={48} height={48} src={client.UPLOAD_URL + comment.authorInfo?.avatar} />
-            ) : (
-                <Avatar size={48} style={{ background: '#7b65f3', color: '#fff' }}>
-                  {comment.authorInfo?.name[0]}
-                </Avatar>
-            )} */}
-            <Avatar
-              size={48}
-              image={comment.authorInfo?.avatar}
-              text={comment.authorInfo?.name[0]}
-            />
-          </div>
-          <div className={styles.comment__content}>
-            <div className={styles.comment__author}>{comment.authorInfo?.name}</div>
-            <div className={styles.comment__text}>{comment.content}</div>
-            <div className={styles.comment__time}>
-              {new Date(comment.createdAt).toLocaleDateString()}
-            </div>
-          </div>
-
-          {/* {comment.authorInfo._id === user._id && (
-            <div className={styles.comment__delete} onClick={() => deleteComment(comment._id)}>
-              <Close color="#f00" />
-            </div>
-        )} */}
-        </div>
-      </div>
+      <MangaComment key={comment._id} comment={comment} />
     ));
   }
 
@@ -105,16 +54,9 @@ const MangaComments = ({ className, comments = [], createComment }) => {
             onInput={handleChange}
             ref={inputRef}></div>
         </div>
-        {/* <p className={messageError ? styles.messageError : styles.notError}>{messageError}</p> */}
         <button className={styles.comments__sendButton} onClick={onSubmit}>
           <Send color={'#8E8E93'} />
         </button>
-
-        {/* <Input sm placeholder="Input comment" onChange={setNewComment} ref={inputRef} /> */}
-
-        {/* <Button rounded onClick={createComment}>
-          Send
-        </Button> */}
       </div>
       <div className={styles.comments__title}>Comments</div>
       <div>{commentsElements.length > 0 ? commentsElements : 'There is no comments yet...'}</div>
