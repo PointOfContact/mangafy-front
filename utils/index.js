d0ef7f3614a5bacfa4551297e46fe71dbd08e2fd
Aren-Gevorgyan <areng4518@gmail.com>
2022-10-13 16:30:09 +0400

    Write logic for view Shots, Episodes, Projects
    Created logickView, viewShot, viewMangaFun, viewChapterFun funs.

---
+++ utils/index.js
@@ -0,0 +1,32 @@
+import { editGallery, viewManga } from 'components/gallery/utils';
+import API from 'features/mangaStory/mangaStoryAPI';
+import getDeviceId from './deviceId';
+
+const logickView = async (user, data, id, key) => {
+  const userId = user?._id || (await getDeviceId(() => {}));
+  const existUser = data?.viewerId.some((value) => String(value) === String(userId));
+  const view = data?.view + 1;
+  const changeData = { view };
+  changeData[key] = id;
+  if (!existUser) changeData.viewerId = [...data?.viewerId, userId];
+  return changeData;
+};
+
+export const viewShot = async (user, data) => {
+  const changeData = await logickView(user, data, data.authorId, 'authorId');
+  editGallery(data._id, changeData);
+};
+
+export const viewMangaFun = async (user, data, mangaStoryId) => {
+  const changeData = await logickView(user, data, mangaStoryId, 'mangaStoryId');
+  viewManga(data._id, changeData);
+};
+
+export const viewChapterFun = async (user, data) => {
+  const userId = user?._id || (await getDeviceId(() => {}));
+  const existUser = data?.viewDeviceUnique.some((value) => String(value) === String(userId));
+  const view = data?.view + 1;
+  const changeData = { view, storyBoardId: data.storyBoardId };
+  if (!existUser) changeData.viewDeviceUnique = [...data?.viewDeviceUnique, userId];
+  API.chapter.patch(data._id, changeData);
+};
