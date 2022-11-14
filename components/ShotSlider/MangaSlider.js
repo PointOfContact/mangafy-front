9f4220aab6927201b769e5df6f3513d883529b84
Vafflee <chelovekosa@gmail.com>
2022-11-14 17:06:20 +0300

    Added useMemo for shotsElements

--- components/ShotSlider/MangaSlider.js
+++ components/ShotSlider/MangaSlider.js
@@ -1,7 +1,7 @@
 import client from 'api/client';
 import ArrowDown2 from 'components/icon/new/ArrowDown2';
 import Imgix from 'components/imgix';
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useMemo } from 'react';
 import styles from './styles.module.scss';
 import { Avatar } from 'antd';
 import cn from 'classnames';
@@ -15,29 +15,38 @@ const MangaSlider = ({ manga, activeChapterIndex }) => {
   const sliderRef = useRef(null);
   const activeChapterRef = useRef(null);
 
-  const shotsElements = manga?.chapters
-    ?.filter((ch) => ch.published)
-    .map((ch, i) => {
-      return (
-        <Link key={i} href={'/manga-view/' + manga?.id + '?ongoing=' + (i + 1)}>
-          <a
-            ref={i + 1 === activeChapterIndex ? activeChapterRef : null}
-            className={cn(
-              styles.slider__item,
-              i + 1 === activeChapterIndex && styles.slider__item_active
-            )}>
-            {ch?.cover ? (
-              <Imgix width={96} height={96} objectFit="cover" src={client.UPLOAD_URL + ch?.cover} />
-            ) : (
-              <Avatar size={96} style={{ background: '#7B65F3', color: '#fff' }}>
-                {ch?.title?.length > 8 ? ch?.title?.slice(0, 8) + '...' : ch?.title}
-              </Avatar>
-            )}
-            <div className={styles.slider__itemTitle}>Chapter {ch.order}</div>
-          </a>
-        </Link>
-      );
-    });
+  const shotsElements = useMemo(
+    () =>
+      manga?.chapters
+        ?.filter((ch) => ch.published)
+        .map((ch, i) => {
+          return (
+            <Link key={i} href={'/manga-view/' + manga?.id + '?ongoing=' + (i + 1)}>
+              <a
+                ref={i + 1 === activeChapterIndex ? activeChapterRef : null}
+                className={cn(
+                  styles.slider__item,
+                  i + 1 === activeChapterIndex && styles.slider__item_active
+                )}>
+                {ch?.cover ? (
+                  <Imgix
+                    width={96}
+                    height={96}
+                    objectFit="cover"
+                    src={client.UPLOAD_URL + ch?.cover}
+                  />
+                ) : (
+                  <Avatar size={96} style={{ background: '#7B65F3', color: '#fff' }}>
+                    {ch?.title?.length > 8 ? ch?.title?.slice(0, 8) + '...' : ch?.title}
+                  </Avatar>
+                )}
+                <div className={styles.slider__itemTitle}>Chapter {ch.order}</div>
+              </a>
+            </Link>
+          );
+        }),
+    [manga?.chapters, activeChapterIndex]
+  );
 
   function scrollToActiveShot() {
     if (activeChapterRef.current && sliderRef.current) {
